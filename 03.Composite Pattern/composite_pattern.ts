72ce24043234a443e8135a1464f6b7695d8091ab
tyagi <tyagi@PC1-11-0022.local>
2013-01-10 13:05:26 +0900

    implement Composite Pattern a little bit.

--- 03.Composite Pattern/composite_pattern.ts
+++ 03.Composite Pattern/composite_pattern.ts
@@ -1,13 +1,55 @@
+//Composite Pattern
+
+//component
 interface Component{
 	add(child:Component):bool;
+	remove(child:Component):bool;
+	getChild():Component[];
+	getName():string;
+	isItem():bool;
 }
+//leaf
 class Item implements Component {
+	constructor(private name: string){
+
+	}
 	add(child:Component):bool{
 		return false;
 	}
+	remove(child:Component):bool{
+		return false;
+	}
+	getChild():Component[]{
+		return null;
+	}
+	getName():string{
+		return this.name;
+	}
+	isItem():bool{
+		return true;
+	}
 }
+//composite
 class Directory implements Component{
+	items:Item[];
+	constructor(private name: string){
+
+	}
 	add(child:Component):bool{
 		return false;
 	}
+	remove(child:Component):bool{
+		return false;
+	}
+	getChild():Component[]{
+		return null;
+	}
+	getName():string{
+		return this.name;
+	}
+	isItem():bool{
+		return false;
+	}
 }
+
+
