af783382a9160b997ddd4ecc6f6b6920ddce08b0
tyagi <tyagi@PC1-11-0022.local>
2013-01-10 15:18:18 +0900

    implemeted composite pattern.

--- 03.Composite Pattern/composite_pattern.ts
+++ 03.Composite Pattern/composite_pattern.ts
@@ -2,54 +2,61 @@
 
 //component
 interface Component{
-	add(child:Component):bool;
-	remove(child:Component):bool;
-	getChild():Component[];
-	getName():string;
-	isItem():bool;
+  add(child:Component):bool;
+  remove(child:Component):bool;
+  getChildren():Component[];
+  getName():string;
+  isItem():bool;
 }
 //leaf
 class Item implements Component {
-	constructor(private name: string){
+  constructor(private name: string){
 
-	}
-	add(child:Component):bool{
-		return false;
-	}
-	remove(child:Component):bool{
-		return false;
-	}
-	getChild():Component[]{
-		return null;
-	}
-	getName():string{
-		return this.name;
-	}
-	isItem():bool{
-		return true;
-	}
+  }
+  add(child:Component):bool{
+    throw new Error("this object is Item.");
+  }
+  remove(child:Component):bool{
+    throw new Error("this object is Item.");
+  }
+  getChildren():Component[]{
+    throw new Error("this object is Item.");
+  }
+  getName():string{
+    return this.name;
+  }
+  isItem():bool{
+    return true;
+  }
 }
 //composite
 class Directory implements Component{
-	items:Item[];
-	constructor(private name: string){
-
-	}
-	add(child:Component):bool{
-		return false;
-	}
-	remove(child:Component):bool{
-		return false;
-	}
-	getChild():Component[]{
-		return null;
-	}
-	getName():string{
-		return this.name;
-	}
-	isItem():bool{
-		return false;
-	}
+  items:Component[];
+  constructor(private name: string){
+    this.items = new Array();
+  }
+  add(child:Component):bool{
+    this.items.push(child);
+    return true;
+  }
+  remove(child:Component):bool{
+    for (var i : number = this.items.length - 1; i >= 0; i--) {
+      if(this.items[i].getName() === child.getName()){
+        this.items.splice(i, 1);
+        i++;
+      }
+    }
+    return true;
+  }
+  getChildren():Component[]{
+    return this.items;
+  }
+  getName():string{
+    return this.name;
+  }
+  isItem():bool{
+    return false;
+  }
 }
 
 
