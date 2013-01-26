2123d87856242b4b4ab59acdee0287fee5439a2c
yagi <sylc.yagi@gmail.com>
2013-01-26 16:17:42 +0900

    modified adapter_pattern. use module.

--- src/01.Adapter Pattern/adapter_pattern.ts
+++ src/01.Adapter Pattern/adapter_pattern.ts
@@ -1,58 +1,58 @@
-
-interface Logger{
-  log(msg: string): void;
-}
-enum LOGGER_TYPE{
-  CONSOLE,
-  ALERT,
-  DISPLAY,
-}
-class LogAdapterFactory{
-  static createLogger(type:LOGGER_TYPE): Logger{
-    switch(type){
-    case LOGGER_TYPE.CONSOLE:
-      return new ConsoleLogger();
-    case LOGGER_TYPE.ALERT:
-      return new AlertLogger();
-    case LOGGER_TYPE.DISPLAY:
-      return new DisplayLogger();
+module Log{
+  export interface Logger{
+    log(msg: string): void;
+  }
+  export enum TYPE{
+    CONSOLE,
+    ALERT,
+    DISPLAY,
+  }
+  export class Factory{
+    static createLogger(type:TYPE): Logger{
+      switch(type){
+      case TYPE.CONSOLE:
+        return new ConsoleLogger();
+      case TYPE.ALERT:
+        return new AlertLogger();
+      case TYPE.DISPLAY:
+        return new DisplayLogger();
+      }
+      return null;
     }
-    return null;
   }
-}
-class ConsoleLogger implements Logger{
-  log(msg:string){
-    console.log(msg);
-  }
-}
-class AlertLogger implements Logger{
-  log(msg:string){
-    alert(msg);    
-  }
-}
-class DisplayLogger implements Logger{
-  display:HTMLDivElement;
-  count:number;
-  constructor(){
-    this.count = 0;
-    this.display = <HTMLDivElement>document.createElement("div");
-    this.display.style.position="absolute";
-    this.display.style.top="0px";
-    this.display.style.right="0px";
-    this.display.style.width="200px";
-    this.display.style.background="#ddffdd";
-    this.display.style.padding="10px";
-    this.display.style.fontSize="10px";
-    document.body.appendChild(this.display);
-  }
-  log(msg:string){
-    if(this.count > 10){
-      this.display.removeChild(this.display.childNodes[0]);
+  class ConsoleLogger implements Logger{
+    log(msg:string){
+      console.log(msg);
     }
-    else{
-      this.count++;
+  }
+  class AlertLogger implements Logger{
+    log(msg:string){
+      alert(msg);    
     }
-    this.display.innerHTML += "<div>"+msg+"</div>";
+  }
+  class DisplayLogger implements Logger{
+    display:HTMLDivElement;
+    count:number;
+    constructor(){
+      this.count = 0;
+      this.display = <HTMLDivElement>document.createElement("div");
+      this.display.style.position="absolute";
+      this.display.style.top="0px";
+      this.display.style.right="0px";
+      this.display.style.width="200px";
+      this.display.style.background="#ddffdd";
+      this.display.style.padding="10px";
+      this.display.style.fontSize="10px";
+      document.body.appendChild(this.display);
+    }
+    log(msg:string){
+      if(this.count > 10){
+        this.display.removeChild(this.display.childNodes[0]);
+      }
+      else{
+        this.count++;
+      }
+      this.display.innerHTML += "<div>"+msg+"</div>";
+    }
   }
 }
-
